d8633a05c081c0e4f6b7077a9dfbaef50ca3577b
Alain Benbassat <alain@businessandcode.eu>
2016-10-21 12:25:05 +0200

    edit org toegevoegd

--- MyCavariaOrganization.php
+++ MyCavariaOrganization.php
@@ -15,6 +15,7 @@ class MyCavariaOrganization {
     $contact = civicrm_api3('Contact', 'getsingle', array(
       'id' => $id,
       'contact_type' => 'Organization',
+      'return' => 'id,hash,display_name,street_address,supplemental_address_1,supplemental_address_2,postal_code,city',
     ));
 
     $this->id = $contact['id'];
@@ -25,5 +26,9 @@ class MyCavariaOrganization {
     $this->addressLine2 = $contact['supplemental_address_2'];
     $this->postalCode = $contact['postal_code'];
     $this->city = $contact['city'];
+  }
+
+  public function update($newValues) {
+
   }
 }
